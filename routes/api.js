7a14e851d5e73447db0d594c684dce6c6a1c8b38
Juan Casas <jcasas@midtronics.com>
2020-05-18 00:48:53 -0500

    Added more stuff to ConfigFile and added translations and print files.

--- routes/api.js
+++ routes/api.js
@@ -1,26 +1,132 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+
+const internalPrintFormatList = {
+  formats: [
+    {
+      type: "InVehicleChargeTest",
+      filename: "in_vehicle_print_format.json"
+    },
+    {
+      type: "OutOfVehicleChargeTest",
+      filename: "out_of_vehicle_print_format.json"
+    },
+    {
+      type: "BatteryDiagnostics",
+      filename: "battery_diagnostics_print_format.json"
+    },
+    {
+      type: "ChargeTest",
+      filename: "charge_test_print_format.json"
+    },
+    {
+      type: "SystemTest",
+      filename: "system_test_print_format.json"
+    },
+    {
+      type: "Footer",
+      filename: "footer_print_format.json"
+    }
+  ]
+};
+
+const boostVoltageSchema = {
+  inVehicle: {
+    chemistry: 120,
+    voltage: 200
+  },
+  outOfVehicle: {
+    chemistry: 130,
+    voltage: 220
+  }
+};
+
+const customerLogo = {
+  onScreen: ["onScreenLogo.png"],
+  bluetoothPrint: ["bluetoothPrintLogo.png"],
+  internalPrint: ["internalPrintLogo.png"],
+  wifiPrint: ["wifiPrintLogo.png"],
+  email: ["emailLogo.png"],
+};
+
+const testCodes = {
+  testCodeFiles: [
+    {
+      testType: 1,
+      fileName: 'fake_test_code_file.json'
+    },
+    {
+      testType: 2,
+      fileName: 'fake_test_code_file.json'
+    }
+  ],
+  testCodeRules: [
+    {
+      testType: 1,
+      rules: [
+        {
+          ruleType: 0,
+          applicationTypes: [
+            1,
+            2
+          ],
+          usages: [
+            1,
+            2,
+            4
+          ],
+          values: [
+            1,
+            3
+          ]
+        }
+      ]
+    },
+    {
+      testType: 2,
+      rules: [
+        {
+          ruleType: 0,
+          applicationTypes: [
+            1,
+            2
+          ],
+          usages: [
+            1,
+            2,
+            4
+          ],
+          values: [
+            1,
+            3
+          ]
+        }
+      ]
+    },
+  ]
+};
+
+const bmisConfiguration = {
+  username: "BmisEncryptedUsername",
+  password: "BmisEncryptedPassword"
+};
+
+const configFileJson = {
+  version: 1,
+  partNumber: "APartNumber-123",
+  translationDatabase: "translations.db",
+  internalPrintFormatList,
+  boostVoltage: boostVoltageSchema,
+  deepDischargeMaxTime: 120,
+  customerLogo,
+  testCodes,
+  bmisConfiguration
+};
 
 /* GET users listing. */
-router.get('/api/config', function(req, res, next) {
-  res.send({
-    version: 1,
-    partNumber: "Example",
-    internalPrintFormat: [
-      {
-        "type": "InVehicleChargeTest",
-        "filename": "a_json1.json"
-      },
-      {
-        "type": " OutOfVehicleChargeTest",
-        "filename": "a_json2.json"
-      },
-      {
-        "type": "BatteryDiagnostics",
-        "filename": "a_json.json"
-      }
-    ]
-  });
+router.get('/api/config', (req, res, next) => {
+  res.send(configFileJson);
 });
 
 router.get('/api/config/configVersion', (req, res, next) => {
@@ -30,15 +136,36 @@ router.get('/api/config/configVersion', (req, res, next) => {
 });
 
 router.get('/getFile', (req, res, next) => {
-  if (req.query.fileName === "a_json1.json") {
-    res.sendFile('a_json1.json', { root: path.join(__dirname, '../public') });
-  } else if (req.query.fileName === "a_json2.json") {
-    res.sendFile('a_json2.json', { root: path.join(__dirname, '../public') });
-  } else if (req.query.fileName === "a_json3.json") {
-    res.sendFile('a_json3.json', { root: path.join(__dirname, '../public') });
-  } else {
-    res.sendFile('a_json1.json', { root: path.join(__dirname, '../public') });
+  const fileName = req.query.fileName;
+  switch(fileName) {
+    case "in_vehicle_print_format.json":
+      sendFile(res, "fake_print_json.json");
+      break;
+    case "out_of_vehicle_print_format.json":
+      sendFile(res, "fake_print_json.json");
+      break;
+    case "battery_diagnostics_print_format.json":
+      sendFile(res, "fake_print_json.json");
+      break;
+    case "charge_test_print_format.json":
+      sendFile(res, "fake_print_json.json");
+      break;
+    case "system_test_print_format.json":
+      sendFile(res, "fake_print_json.json");
+      break;
+    case "footer_print_format.json":
+      sendFile(res, "fake_print_json.json");
+      break;
+    case "translations.db":
+      sendFile(res, "translations.db");
+      break;
+    default:
+      res.status(404).send({ success: false });
   }
 });
+
+const sendFile = (res, fileName) => {
+  res.sendFile(path.join(__dirname, '../public/' + fileName));
+};
 
 module.exports = router;
